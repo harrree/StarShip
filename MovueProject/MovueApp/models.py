35749b3ce1b2daa840ed7b94f7de326039738fa6
febin <febinsunny2004@gmail.com>
2025-02-19 15:40:12 +0530

    my first commit

--- MovueProject/MovueApp/models.py
+++ MovueProject/MovueApp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userid=models.AutoField(primary_key=True)
+    username=models.TextField(max_length=500)
+    email=models.EmailField(max_length=250)
+    password=models.CharField(max_length=200)
+
+
+class TableMovie(models.Model):
+    movieid=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=200)
+    description=models.TextField(max_length=500)
+    releasedate=models.DateField()
+
+class TableGenre(models.Model):
+    genreid=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=200)
+
+class MovieGenre(models.Model):
+    id=models.AutoField(primary_key=True)
+    movieid=models.ForeignKey(TableMovie,on_delete=models.CASCADE)
+    genreid=models.ForeignKey(TableGenre,on_delete=models.CASCADE)
+
+class Review(models.Model):
+    reviewid=models.AutoField(primary_key=True)
+    movieid=models.ForeignKey(TableMovie,on_delete=models.CASCADE)
+    genreid=models.ForeignKey(TableGenre,on_delete=models.CASCADE)    
+    reviewtext=models.TextField(max_length=500)
+    timestamp=models.DateTimeField()
+    ratingscore=models.PositiveSmallIntegerField()
+
+class Reaction(models.Model):
+    reactionid=models.AutoField(primary_key=True)
+    userid=models.ForeignKey(User,on_delete=models.CASCADE)
+    reviewid=models.ForeignKey(Review,on_delete=models.CASCADE)
+    reactiontype=models.TextField()
+
+class Watchtable(models.Model):
+    watchid=models.AutoField(primary_key=True)
+    userid=models.ForeignKey(User,on_delete=models.CASCADE)  
+    movieid=models.ForeignKey(TableMovie,on_delete=models.CASCADE)  
+    addeddate=models.DateTimeField()
+
+    
+    
+
--- MovueProject/MovueProject/settings.py
+++ MovueProject/MovueProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'MovueApp.apps.MovueappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
