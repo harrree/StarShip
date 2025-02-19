362f6062184e6b5fd91287b456b9eed87e2f06f7
febin <febinsunny2004@gmail.com>
2025-02-19 15:57:44 +0530

    my corrected commit

--- MovueProject/MovueApp/admin.py
+++ MovueProject/MovueApp/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from .models import User,TableMovie,TableGenre,MovieGenre,Review,Reaction,Watchtable
+from django.contrib import admin # type: ignore
+from .models import TableMovie,TableGenre,MovieGenre,Review,Reaction,Watchtable
 
 # Register your models here.
-admin.site.register(User)
+
 admin.site.register(TableMovie)
 admin.site.register(TableGenre)
 admin.site.register(MovieGenre)
