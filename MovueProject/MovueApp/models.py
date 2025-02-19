223e8a3bf5e61f295eb771943a637c068d79b3ca
febin <febinsunny2004@gmail.com>
2025-02-19 15:50:49 +0530

    my user import

--- MovueProject/MovueApp/models.py
+++ MovueProject/MovueApp/models.py
@@ -1,11 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class User(models.Model):
-    userid=models.AutoField(primary_key=True)
-    username=models.TextField(max_length=500)
-    email=models.EmailField(max_length=250)
-    password=models.CharField(max_length=200)
 
 
 class TableMovie(models.Model):
