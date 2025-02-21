de81ac5bf18567fa3bfb18cc584a9e180f27d871
febin <febinsunny2004@gmail.com>
2025-02-21 15:52:07 +0530

    committing first two views

--- MovueProject/MovueApp/views.py
+++ MovueProject/MovueApp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Movie
+
 
 # Create your views here.
+def movie_list(request):
+    move=Movie.objects.all()
+    context={"list":move}
+    return render(request,"index.html",context)
+
+
+def information(request,id):
+    
+    movi=Movie.objects.filter(movieid=id).values()
+    
+    context={'key':movi}
+    return render(request,'movie_list.html',context)
+
+ 
