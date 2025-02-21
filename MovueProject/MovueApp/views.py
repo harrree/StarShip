07cdf45f060ee1d2cca1e8a97b8793743d8a7eb8
febin <febinsunny2004@gmail.com>
2025-02-22 01:50:28 +0530

    commit on movie details

--- MovueProject/MovueApp/views.py
+++ MovueProject/MovueApp/views.py
@@ -5,15 +5,19 @@ from .models import Movie
 # Create your views here.
 def movie_list(request):
     move=Movie.objects.all()
+   
     context={"list":move}
     return render(request,"index.html",context)
 
 
 def information(request,id):
     
-    movi=Movie.objects.filter(movieid=id).values()
+    movies=Movie.objects.get(movieid=id)
+
+    genre=movies. genre.all()
+    print( movies. genre.all())
     
-    context={'key':movi}
+    context={"movies": movies,"movie_genres":  genre }
     return render(request,'movie_list.html',context)
 
  
