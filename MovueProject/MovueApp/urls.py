5157703e6409d17b39e7faf79608a0b66d69d47b
febin <febinsunny2004@gmail.com>
2025-02-24 22:04:06 +0530

    not an sucessfull commit

--- MovueProject/MovueApp/urls.py
+++ MovueProject/MovueApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.movie_list,name='movie_list'),
-    path('mov/<int:id>',views.information,name="information"),
+    path('mov/<int:id>/',views.information,name="information"),
+    path('rev/<int:id>',views.rev,name='rev')
 ]
--- MovueProject/MovueApp/views.py
+++ MovueProject/MovueApp/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render,redirect
-from .models import Movie
+from .models import Movie,ReviewRating,User
+from django.http import HttpResponse
 
 
 # Create your views here.
+#function created for movie list
 def movie_list(request):
     move=Movie.objects.all()
    
     context={"list":move}
     return render(request,"index.html",context)
 
-
+#function for getting information about specific movie
 def information(request,id):
     
     movies=Movie.objects.get(movieid=id)
+    
 
     genre=movies. genre.all()
     print( movies. genre.all())
@@ -20,4 +23,14 @@ def information(request,id):
     context={"movies": movies,"movie_genres":  genre }
     return render(request,'movie_list.html',context)
 
- 
+#fuction created for review
+
+def rev(request,id):
+
+    review=ReviewRating.objects.filter(movieid=id).values()
+    
+    context={"review":review}
+    print(context)
+    return render(request,'movie_list.html',context)    
+
+
